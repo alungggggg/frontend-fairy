919738415a1fcf5c7b7d8a5c846c3d57a4b144fc
Dani Ardiansyach <ardiansyachd5@gmail.com>
2024-07-25 01:37:45 +0700

    test

--- src/Pages/Admin/updateDongeng.jsx
+++ src/Pages/Admin/updateDongeng.jsx
@@ -1,15 +1,15 @@
 const updateDongeng = () => {
-    return (
-        <>
-            <form action="">
-                <label htmlFor="">Title</label>
-                <input type="text" />
+  return (
+    <>
+      <form action="">
+        <label htmlFor="">Title</label>
+        <input type="text" />
 
-                <label htmlFor="">File Pdf</label>
-                <input type="file" />
-            </form>
-        </>
-    )
-}
+        <label htmlFor="">File Pdf</label>
+        <input type="file" />
+      </form>
+    </>
+  );
+};
 
 export default updateDongeng;
