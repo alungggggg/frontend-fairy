80be23c2d3d683b97704f96f745357848280abc1
Dani Ardiansyach <ardiansyachd5@gmail.com>
2024-09-11 14:03:19 +0700

    Update Profile

--- src/Pages/Services/profile.jsx
+++ src/Pages/Services/profile.jsx
@@ -33,40 +33,46 @@ const profile = () => {
     <>
       <UserLayout>
         <section className="container mt-5 mb-5">
-          <section className="row">
-            <section className="col-4 bg-primary-subtle"></section>
-            <section className="col-8 p-0 bg-white">
+          <section className="row p-3">
+            <section className="col-sm-12 col-md-4 bg-secondary-subtle d-flex justify-content-center align-items-center">
+              <img
+                src="https://th.bing.com/th/id/OIP.oVIyTk_GGnAj3YzNXppdpQAAAA?w=189&h=189&c=7&r=0&o=5&pid=1.7"
+                alt=""
+                className="rounded-circle p-md-0 p-3 "
+              />
+            </section>
+            <section className=" col-sm-12 col-md-8 p-0 bg-white">
               <p className="text-center border-bottom border-secondary-subtle fw-bold p-2">
                 Information
               </p>
-              <section className="row text-center ">
-                <section className="col-6 p-0">
-                  <p className="fw-bold mb-0">Nama</p>
+              <section className="d-flex text-center ">
+                <section className="w-50 p-0">
+                  <p className=" fw-bold mb-0">Nama</p>
                   <p className="text-body-secondary">{data.nama}</p>
                 </section>
-                <section className="col-6">
+                <section className="w-50">
                   <p className="fw-bold mb-0">Username</p>
                   <p className="text-body-secondary">{data?.username}</p>
                 </section>
               </section>
-              <section className="row text-center ">
-                <section className="col-6">
+              <section className="d-flex text-center ">
+                <section className="w-50">
                   <p className="fw-bold mb-0">Email</p>
                   <p className="text-body-secondary">{data?.email}</p>
                 </section>
-                <section className="col-6">
+                <section className="w-50">
                   <p className="fw-bold mb-0">Role</p>
                   <p className="text-body-secondary">{data.role}</p>
                 </section>
               </section>
               {data.role == "siswa" ? (
                 <>
-                  <section className="row text-center border-bottom border-secondary-subtle">
-                    <section className="col-6">
+                  <section className="d-flex text-center border-bottom border-secondary-subtle">
+                    <section className="w-50">
                       <p className="fw-bold mb-0">Kelas</p>
                       <p className="text-body-secondary">{data?.kelas}</p>
                     </section>
-                    <section className="col-6">
+                    <section className="w-50">
                       <p className="fw-bold mb-0">Sekolah</p>
                       <p className="text-body-secondary">{data.sekolah}</p>
                     </section>
@@ -75,7 +81,7 @@ const profile = () => {
               ) : (
                 ""
               )}
-              <section className="p-3">
+              <section className="p-3 d-flex justify-content-end">
                 <a href="/profile/update" className="btn btn-orange text-white">
                   Update Profile
                 </a>
