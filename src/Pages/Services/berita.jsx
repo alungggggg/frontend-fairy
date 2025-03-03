bbe42020c8c91003ce3b7fd3b2b37fc41a26ce16
Dani Ardiansyach <ardiansyachd5@gmail.com>
2025-03-03 17:15:16 +0700

    Update Berita

--- src/Pages/Services/berita.jsx
+++ src/Pages/Services/berita.jsx
@@ -24,64 +24,81 @@ const Berita = () => {
     <>
       <Header />
       <div className="container my-5">
-        {isLoading ? (
-          <section
-            className="d-flex align-items-center justify-content-center w-full"
-            style={{ height: "80vh" }}
-          >
-            <Loading />
-          </section>
-        ) : (
-          <>
-            <div>{/* <h1>Berita Terkini Anjay</h1> */}</div>
-            <section className="py-2 position-relative w">
-              <section className="">
-                <section className="row align-items-center">
-                  <section className="col-lg-6">
-                    <h3 className="fw-bold">
-                      Berita&nbsp;
-                      <section className="d-inline-flex flex-column">
-                        <span>untuk semua</span>
-                        <span className="mtmin">
-                          <img
-                            src="https://buku.kemdikbud.go.id/assets/image/home/line-populer.png"
-                            alt="Line title"
-                          />
-                        </span>
-                      </section>
-                    </h3>
-                    <p className="">Temukan berita sesuai kebutuhanmu</p>
+        <div className="container my-5">
+          {isLoading ? (
+            <section
+              className="d-flex align-items-center justify-content-center w-full"
+              style={{ height: "80vh" }}
+            >
+              <Loading />
+            </section>
+          ) : berita?.length > 0 ? (
+            <>
+              <div>{/* <h1>Berita Terkini Anjay</h1> */}</div>
+              <section className="py-2 position-relative w">
+                <section className="">
+                  <section className="row align-items-center">
+                    <section className="col-lg-6">
+                      <h3 className="fw-bold">
+                        Berita&nbsp;
+                        <section className="d-inline-flex flex-column">
+                          <span>untuk semua</span>
+                          <span className="mtmin">
+                            <img
+                              src="https://buku.kemdikbud.go.id/assets/image/home/line-populer.png"
+                              alt="Line title"
+                            />
+                          </span>
+                        </section>
+                      </h3>
+                      <p className="">Temukan berita sesuai kebutuhanmu</p>
+                    </section>
+                    <section className="col-lg-6 text-end"></section>
                   </section>
-                  <section className="col-lg-6 text-end"></section>
-                </section>
 
-                <section className="row gy-4">
-                  {berita.map((item, index) => (
-                    <Link className="col-lg-4 d-flex" key={index} to={"./"+item.id}>
-                      <section className="card shadow d-flex flex-column h-100 w-100">
-                        <img
-                          src={import.meta.env.VITE_IMG_URL+"/"+item.gambar}
-                          style={{
-                            width: "100%",
-                            height: "400px",
-                            objectFit: "contain",
-                          }}
-                          alt={item.judul}
-                        />
-                        <div className="card-body d-flex flex-column flex-grow-1">
-                          <h3 className="text-center text-decoration-none">{item.judul}</h3>
-                          <p className="card-text text-truncate flex-grow-1 text-decoration-none">
-                            {item?.description || ""}
-                          </p>
-                        </div>
-                      </section>
-                    </Link>
-                  ))}
+                  <section className="row gy-4">
+                    {berita.map((item, index) => (
+                      <Link
+                        className="col-lg-4 d-flex"
+                        key={index}
+                        to={"./" + item.id}
+                      >
+                        <section className="card shadow d-flex flex-column h-100 w-100">
+                          <img
+                            src={
+                              import.meta.env.VITE_IMG_URL + "/" + item.gambar
+                            }
+                            style={{
+                              width: "100%",
+                              height: "400px",
+                              objectFit: "contain",
+                            }}
+                            alt={item.judul}
+                          />
+                          <div className="card-body d-flex flex-column flex-grow-1">
+                            <h3 className="text-center text-decoration-none">
+                              {item.judul}
+                            </h3>
+                            <p className="card-text text-truncate flex-grow-1 text-decoration-none">
+                              {item?.description || ""}
+                            </p>
+                          </div>
+                        </section>
+                      </Link>
+                    ))}
+                  </section>
                 </section>
               </section>
-            </section>
-          </>
-        )}
+            </>
+          ) : (
+            <h1
+              className="text-center"
+              style={{ marginTop: "200px", marginBottom: "200px" }}
+            >
+              Belum ada berita
+            </h1>
+          )}
+        </div>
       </div>
       <Footer />
     </>
--- src/Pages/Services/detailBerita.jsx
+++ src/Pages/Services/detailBerita.jsx
@@ -31,10 +31,12 @@ const DetailBerita = () => {
     handleGetDataBeritaById();
   }, []);
 
+  console.log(dataBerita);
+
   return (
     <>
       <Header></Header>
-      {/* <div className="container my-5">
+      <div className="container my-5">
         {isLoading ? (
           <section
             className="d-flex align-items-center justify-content-center w-full"
@@ -79,77 +81,13 @@ const DetailBerita = () => {
                       alt={item?.judul || ""}
                     />
                   </div>
-                  <div 
-                  
-                  className="ms-3">
+                  <div className="ms-3">
                     <h4 className="m-0">{item?.judul || ""}</h4>
                   </div>
                 </div>
               ))}
             </div>
           </div>
-        )}
-      </div> */}
-      <div className="container my-5">
-        {isLoading ? (
-          <section
-            className="d-flex align-items-center justify-content-center w-full"
-            style={{ height: "80vh" }}
-          >
-            <Loading />
-          </section>
-        ) : dataBerita?.length > 0 ? (
-          <div className="row gy-5">
-            <div className="col-lg-8">
-              <div>
-                <img
-                  src={
-                    import.meta.env.VITE_IMG_URL + "/" + dataBerita[0]?.gambar
-                  }
-                  className="img-fluid w-100 rounded"
-                  style={{ height: "300px", objectFit: "contain" }}
-                  alt={dataBerita[0]?.judul || "Gambar tidak tersedia"}
-                />
-                <h1 className="text-center">{dataBerita[0]?.judul}</h1>
-                <p>{dataBerita[0]?.description}</p>
-              </div>
-            </div>
-            <div className="col-lg-4 card shadow">
-              {rekomendasiBerita?.length > 0 ? (
-                rekomendasiBerita.map((item, i) => (
-                  <div className="d-flex my-3 align-items-center" key={i}>
-                    <div
-                      style={{
-                        width: "80px",
-                        height: "80px",
-                        flexShrink: 0,
-                      }}
-                    >
-                      <img
-                        src={import.meta.env.VITE_IMG_URL + "/" + item.gambar}
-                        style={{
-                          width: "100%",
-                          height: "100%",
-                          objectFit: "contain",
-                          borderRadius: "8px",
-                        }}
-                        alt={item?.judul || "Gambar tidak tersedia"}
-                      />
-                    </div>
-                    <div className="ms-3">
-                      <h4 className="m-0">
-                        {item?.judul || "Judul tidak tersedia"}
-                      </h4>
-                    </div>
-                  </div>
-                ))
-              ) : (
-                <p className="text-center">Tidak ada rekomendasi berita</p>
-              )}
-            </div>
-          </div>
-        ) : (
-          <h1 className="text-center">Belum ada berita</h1>
         )}
       </div>
 
